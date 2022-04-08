77816e94f03bdd0aac8c82fdcbbd4d03e9f48f18
Suizer <93068036+Suizer98@users.noreply.github.com>
2022-04-08 14:39:42 +0800

    Add files via upload

---
+++ geoApp/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+import mysql.connector
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('aboutme/', views.about, name='about'),
+    path('explore/', views.explore, name='explore'),
+]
+
